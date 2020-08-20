3c1b0bc29430933c723bad9b27f36c0bb0fcbe1e
Tony Tsx <tony.js@zoho.eu>
2020-08-20 17:55:06 -0300

    upgrade length system metric

--- src/systems/length.ts
+++ src/systems/length.ts
@@ -1,22 +1,40 @@
 import System from '../System'
 
 const length = new System<length.Units>( 'length', 'm' )
-  .add( 'ft', 'Foot', 'Foot', 'multiply', 0.3048 )
-  .add( 'ft-us', 'Foot', 'Foot', 'multiply', 0.3048006096 )
+  .add( 'nm', 'Nanometer', 'Nanometers', 'multiply', 1e+9 )
+  .add( 'μm', 'Micrometer', 'Micrometers', 'multiply', 1e+6 )
+  .add( 'mm', 'Millimeter', 'Millimeters', 'multiply', 1000 )
+  .add( 'cm', 'Centimeter', 'Centimeters', 'multiply', 100 )
+  .add( 'in', 'Inch', 'Inches', 'multiply', 39.37 )
+  .add( 'ft', 'Foot', 'Foots', 'multiply', 3.28084 )
+  .add( 'yd', 'Yard', 'Yards', 'multiply', 1.094 )
+  .unit( 'm', 'Meter', 'Meters' )
   .add( 'km', 'Kilometer', 'Kilometers', 'divide', 1000 )
   .add( 'mi', 'Mile', 'Miles', 'divide', 1609 )
   .add( 'nMi', 'Nautical mile', 'Nautical miles', 'divide', 1852 )
   .add( 'fathom', 'Fathom', 'Fathoms', 'divide', 1.829 )
-  .add( 'mm', 'Millimeter', 'Millimeters', 'multiply', 1000 )
-  .add( 'cm', 'Centimeter', 'Centimeters', 'multiply', 100 )
-  .add( 'in', 'Inch', 'Inches', 'multiply', 39.37 )
-  .unit( 'm', 'Meter', 'Meters' )
 
 namespace length {
-  export type Units = [ 'nMi', 'mi', 'fathom', 'in', 'ft', 'ft-us', 'mm', 'cm', 'm', 'km' ]
+  export type Units = [
+    'nm',
+    'μm',
+    'mm',
+    'cm',
+    'in',
+    'ft',
+    'yd',
+    'm',
+    'km',
+    'mi',
+    'nMi',
+    'fathom',
+  ]
 }
 
 declare global {
+  export interface Unit {
+    ( value: number, unity: System.Unit<length.Units> ): Unit.UnitWithSystem<System<length.Units>>
+  }
   namespace Unit {
     export interface UnitWithoutSystem {
       convert( to: System.Unit<length.Units> ): Unit.UnitWithSystem<System<length.Units>>
